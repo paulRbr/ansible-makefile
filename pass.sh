paulRbr/ansible-makefile

commit e4fe37fd2d23120c4ebd55ae91bd12ec4aa292c9

secrets: improve reading vault passphrase with the pass.sh wrapper
Don't fail if a vault passphrase isn't found in the current password
store.

pass.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -e
+
+env=${env:-}
 
 # ########################
 #
@@ -17,7 +21,15 @@
 #
 # ########################
 
-if [ $(which pass) ];
+if (command -v pass)
 then
-    pass ansible-vault/$env
+    existingVault=$(pass "ansible-vault/${env}" || true)
+
+    if [ -n "${existingVault}" ]
+    then
+        echo "${existingVault}"
+    else
+        >&2 echo "Defaulting to an random vault pass. Don't trust it if you are using vaulted variables!"
+        echo "invalid_vault_pass"
+    fi
 fi

commit 29ac02b1783f6c7173c3d2d2c6635b5743bfc378

fix: pass.sh was outputing pass binary path and breaking its usage
The commit e4fe37f introduced a bug which prevented the pass.sh to
fetch the correct passphrase from password-store.

pass.sh
@@ -21,7 +21,7 @@ env=${env:-}
 #
 # ########################
 
-if (command -v pass)
+if (command -v pass >/dev/null 2>&1)
 then
     existingVault=$(pass "ansible-vault/${env}" || true)
 